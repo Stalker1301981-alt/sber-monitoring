fe41e67e755b5cf89c65643955615bcaa0b0b397
Stalker1301981-alt <Stalker1301981@yandex.ru>
2026-06-09 09:14:22 +0300

    fix: update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,14 @@
-import urllib.request
-resp = urllib.request.urlopen('http://localhost:8080')
-print(resp.read().decode())
+import unittest, sys, os, threading, time
+sys.path.insert(0, os.path.dirname(__file__))
+from calc import SberMonitoringWebsite, APP_VERSION
+from http.server import HTTPServer
+
+class TestApp(unittest.TestCase):
+    def test_version_exists(self):
+        self.assertTrue(len(APP_VERSION) > 0)
+
+    def test_class_exists(self):
+        self.assertTrue(issubclass(SberMonitoringWebsite, object))
+
+if __name__ == '__main__':
+    unittest.main()
